921969610b6464c2ea00651d67abb1373817d2d3
VincentDrummond <vincentdrummond@hotmail.co.uk>
2022-06-24 20:21:13 +0100

    final version

--- week8_database_api.js
+++ week8_database_api.js
@@ -1,43 +1,61 @@
 /************************************************
-GETTING INFO FROM THE API
+GETTING INFO FROM THE API (GNews.io)
 ************************************************/
-const apiKey = 'efbaa39f5d0bac6c07336e1a65dc1ff6'; // GNews.io
-const apiEndpointBreaking = `https://gnews.io/api/v4/search?q=example&token=${apiKey}&language=en`;
-const showNews = document.getElementById('breakingNews');
+const apiKey = 'efbaa39f5d0bac6c07336e1a65dc1ff6';
+const apiEndpoint = `https://gnews.io/api/v4/search?q=example&token=${apiKey}&language=en`;
+const showNews = document.getElementById('theNews');
 
-
-async function getBreakingNews() {
-	
-	try {
-		const apiResponse = await fetch(apiEndpointBreaking);
-
-		if (!apiResponse.ok) {
-			throw apiResponse
-		} else {
-		console.log('Response is ok');
-		};
-
-
-
-	} catch (error) {
-		console.log('There has been an error:', error);
-	}
-};
-
-getBreakingNews();
 
 /************************************************
-RENDERING THE DATA
+A FUNCTION FOR BUILDING PAGE ELEMENTS
 ************************************************/
-// function renderNews(news=[]) {
-// 	const fragment = document.createDocumentFragment();
+function renderArticle(article) {
+	const mainDiv = document.createElement("div")
+	
+	const image = document.createElement("img");
+		image.classList.add('newsImage');
+		image.src = article.image;
 
-// 	for (const news of news)
-// }
+	const h1 = document.createElement("h1");
+		h1.classList.add('h1Text');
+		h1.textContent = article.title;
 
-// const newsData = await apiResponse.json();
-// renderNews(newsData);
+	const divContent = document.createElement("div");
+		divContent.classList.add('articleText');
+		divContent.textContent = article.content;
 
+	const articleURL = document.createElement("a");
+	articleURL.classList.add('url');
+	articleURL.textContent = article.url;
+
+	mainDiv.appendChild(image);
+	mainDiv.appendChild(h1);
+	mainDiv.appendChild(divContent);
+	mainDiv.appendChild(articleURL);
+	showNews.appendChild(mainDiv);
+};
+
+/************************************************
+WHAT'S THIS?
+************************************************/
+
+async function getNews() {
+	try {
+		const apiResponse = await fetch(apiEndpoint);
+
+		if(!apiResponse.ok) {
+			throw apiResponse;
+		}
+
+		const result = await apiResponse.json();
+		result.articles.forEach(article => renderArticle(article))
+
+		console.log("Response is OK", result);
+	} catch (err) {
+		console.log("There has been an error of", err);
+	}
+}
+getNews();
 
 
 /************************************************
